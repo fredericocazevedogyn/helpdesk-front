2714c965e32299569b850fb70eba4aad950db3f0
Frederico Carvalho <dentin15@gmail.com>
2022-06-06 21:13:50 -0300

    serviço chamado-update implementado

--- src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Chamado } from 'src/app/models/chamado';
 import { ChamadoService } from './../../../service/chamado.service';
@@ -42,20 +42,30 @@ export class ChamadoUpdateComponent implements OnInit {
     private tecnicoService: TecnicoService,
     private chamadoService: ChamadoService,
     private toastService:   ToastrService,
-    private router:         Router
+    private router:         Router,
+    private route:          ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
     this.findAllClientes();
     this.findAllTecnicos();
   }
 
-  create(): void {
-    this.chamadoService.create(this.chamado).subscribe(resposta => {
-      this.toastService.success('Chamado criado com sucesso', 'Novo chamado');
+  findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(resposta => {
+      this.chamado = resposta;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  update(): void {
+    this.chamadoService.update(this.chamado).subscribe(resposta => {
+      this.toastService.success('Chamado alterado com sucesso', 'Alterar chamado');
       this.router.navigate(['chamados']);
     }, ex => {
-      console.log(ex)
       this.toastService.error(ex.error.error);
     })
   }
@@ -79,6 +89,26 @@ export class ChamadoUpdateComponent implements OnInit {
             this.observacao.valid &&
             this.tecnico.valid    &&
             this.cliente.valid
+  }
+
+  retornaStatus(status: any): string {
+    if (status == '0'){
+      return 'ABERTO'
+    } else if (status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == '0'){
+      return 'BAIXA'
+    } else if (prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
   }
 
 }
